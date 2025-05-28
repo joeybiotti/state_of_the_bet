d8482e21f0b04cbcf17c529f814e938801936363
Joey Biotti <joeybiotti@Joeys-MacBook-Pro.local>
2025-05-28 17:48:08 -0400

    Enhance Kafka consumer and producer: add contract data handling and fix deserialization issues

--- kafka_pipeline/consumer.py
+++ kafka_pipeline/consumer.py
@@ -1,6 +1,6 @@
 import json
 from kafka import KafkaConsumer
-from db import insert_market_data
+from db import insert_market_data, insert_contract_data
 
 KAFKA_TOPIC = 'predictit_markets'
 KAFKA_BOOTSTRAP_SERVERS = 'localhost:9092'
@@ -10,8 +10,8 @@ consumer = KafkaConsumer(
     KAFKA_TOPIC,
     bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,
     auto_offset_reset='earliest',
-    enable_auto_commits=True,
-    value_deserializer=lambda v: json.loads(v.decode('uft-8'))
+    enable_auto_commit=True,
+    value_deserializer=lambda v: json.loads(v.decode('utf-8'))
 )
 
 # Process Incoming Kafka Messages
@@ -20,9 +20,14 @@ consumer = KafkaConsumer(
 def consume_kafka_data():
     for message in consumer:
         market_data = message.value
-        print(f'Received Market: {market_data}')  # Debugging
+        print(
+            f"Contracts Extracted: {json.dumps(market_data.get('contracts', []), indent=2)}")
 
         insert_market_data(market_data)
+
+        for contract in market_data.get('contracts', []):
+            print(f'Received Contracts: {contract}')
+            insert_contract_data(contract)
 
 
 if __name__ == '__main__':
--- kafka_pipeline/producer.py
+++ kafka_pipeline/producer.py
@@ -1,29 +1,33 @@
-import json 
+import json
 import requests
 from kafka import KafkaProducer
 
 KAFKA_TOPIC = 'predictit_markets'
-KAFKA_BOOTSTRAP_SERVERS= 'localhost:9092'
+KAFKA_BOOTSTRAP_SERVERS = 'localhost:9092'
 
 # Initialize Kafka Producer
 producer = KafkaProducer(
-    bootstrap_servers= KAFKA_BOOTSTRAP_SERVERS,
-    value_serializer = lambda v: json.dumps(v).encode('utf-8') # JSON Serialization 
+    bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,
+    value_serializer=lambda v: json.dumps(
+        v).encode('utf-8')  # JSON Serialization
 )
 
 # Fetch data
+
+
 def get_predictit_data():
     url = 'https://www.predictit.org/api/marketdata/all/'
-    response= requests.get(url)
-    
-    if response.status_code==200:
-        try: 
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        try:
             data = response.json()
             return data['markets']
         except ValueError as e:
             print(f'JSON Parse Error: {e}')
             return None
-    print(f'Failed to fetch PredictIt data. Status Code: {response.status_code}')
+    print(
+        f'Failed to fetch PredictIt data. Status Code: {response.status_code}')
     return None
 
 
@@ -32,14 +36,19 @@ def stream_to_kafka():
     data = get_predictit_data()
     if data:
         for market in data:
-            for contract in market['contracts']:
-                for key, value in contract.items():
-                    if isinstance(value, bytes):
-                        contract[key] = value.decode('utf-8')
-                        
+            if 'contracts' in market:
+                for contract in market['contracts']:
+                    for key, value in contract.items():
+                        if isinstance(value, bytes):
+                            contract[key] = value.decode('utf-8')
+
         producer.send(KAFKA_TOPIC, value=market)
 
         producer.flush()
-        print(f'Sent {len(data)} markets to Kafka.') 
+        print(f'Sent {len(data)} markets to Kafka.')
     else:
         print('Failed to fetch  PredictIt data.')
+
+
+if __name__ == '__main__':
+    stream_to_kafka()
